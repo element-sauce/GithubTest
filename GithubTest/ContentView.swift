841c0058c823765bdcf8892a0bbc183cda20dcbe
Bill Xiang <bill28.eaton@gmail.com>
2020-06-10 23:12:48 -0700

    Initial Commit

---
+++ GithubTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubTest
+//
+//  Created by Bill Xiang on 6/10/20.
+//  Copyright © 2020 billx. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
